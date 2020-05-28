70ee1572642229522b781243c1e00130aecba79d
Bruno Rodrigues <dominusbruno@gmail.com>
2020-05-28 13:25:05 -0300

    Variáveis com let

---
+++ index.js
@@ -0,0 +1,6 @@
+let ele = "Bruno Rodrigues"
+let ela = "Carol Fernandes"
+let filho = "João Vitor"
+let filha = "Sofia Fernandes"
+
+console.log (`${ele} e ${ela} tem 2 filhos, o mais velho chama-se ${filho} e tem 7 anos, a mais nova é ${filha} e tem 4 anos.`)
