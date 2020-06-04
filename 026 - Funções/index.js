e4feefed1624f02991751a522e10ed1b71799601
Bruno Rodrigues <dominusbruno@gmail.com>
2020-06-04 15:16:41 -0300

    Inserido os comentários

--- 026 - Funções/index.js
+++ 026 - Funções/index.js
@@ -1,6 +1,6 @@
-function soma(a,b) {
-    let x = a+b
-    return x
+function soma(a,b) { //Recebe os valores declarados de A e B,
+    let x = a+b // Executa a soma de a+b e atribui à variável X
+    return x //Envia para o console.log o valor calculado acima.
 }
 
-console.log(soma(10,7))
+console.log(soma(10,7)) //Chama a função SOMA e envia para eles os valores 10 para A e 7 para B.
