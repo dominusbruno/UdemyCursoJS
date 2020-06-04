b859f05b5a7cd8d20175df9bf8705ad08f3d6e13
Bruno Rodrigues <dominusbruno@gmail.com>
2020-06-04 15:09:03 -0300

    Variável com let

--- 009 - Variável let/index.js
+++ 009 - Variável let/index.js
@@ -2,6 +2,6 @@ let ele = "Bruno Rodrigues"
 let ela = "Carol Fernandes"
 let filho = "João Vitor"
 let filha = "Sofia Fernandes"
-let familia = "Rodrigues"
+let familia = "Rodrigues" 
 
 console.log (`${ele} e ${ela} tem 2 filhos, o mais velho chama-se ${filho} e tem 7 anos, a mais nova é ${filha} e tem 4 anos. juntos eles formam a família ${familia}.`)
