c82ed2b859b58d0f0169844cdd449eb5b1fab2f6
Bruno Rodrigues <dominusbruno@gmail.com>
2020-06-09 12:18:02 -0300

    Operadores

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+OPERADORES DE COMPARAÇÃO
+
+>       Maior que
+>=      Maior ou igual
+<       Menor que
+<=      Menor ou iqual 
+==      Igual
+===     Igual estrito (valor e tipo)
+!=      Diferente
+!==     Diferente estrito (valor e tipo)
+
+####################################################################
+####################################################################
+
+
+OPERADORES DE LÓGICOS
+
+&&      E
+||      OU
+!       NÃO
+
+*/
