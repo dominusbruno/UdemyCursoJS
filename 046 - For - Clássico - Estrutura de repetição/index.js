c77d1bb76e569ee34dd356e981735ff950aadd4d
Bruno Rodrigues <dominusbruno@gmail.com>
2020-06-18 17:52:34 -0300

    For - Clássico - Estrutura de repetição

---
+++ 046 - For - Clássico - Estrutura de repetição/index.js
@@ -0,0 +1,11 @@
+// Lista os números de 0 a 10
+//for (i = 0; i <= 10; i++) {
+//    console.log(i);
+//}
+
+
+const frutas = ["Maçã", "Pêra", "Uva", "Goiaba", "Melão"]
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`No indice ${i} está a fruta ${frutas[i]}`)
+}
