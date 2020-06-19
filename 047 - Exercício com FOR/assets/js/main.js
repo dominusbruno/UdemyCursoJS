3c00d5a8a4fb8d4ffdf0ba8bb5e56f387db75f4f
Bruno Rodrigues <dominusbruno@gmail.com>
2020-06-19 11:04:49 -0300

    Exercícios com FOR

--- 047 - Exercício com FOR/assets/js/main.js
+++ 047 - Exercício com FOR/assets/js/main.js
@@ -6,21 +6,32 @@ const elementos = [
     {tag: 'footer', texto: 'Frase 04'},
 ];
 
-const container = document.querySelector('.container'); //Seleciona a CLASSE "container"
-const div =document.createElement('div'); //Criamos uma DIV que irá receber todos os demais elementos do array
+//Declaramos a constante "container" para selecionar a "<section class="container">" do HTML
+const container = document.querySelector('.container');
+
+//Declaramos a constante "div" para criar um elemento "<div>" no HTML, aonde ficarão armazenados todos os valores salvos na memoria.
+const div =document.createElement('div');
 
 
 //O FOR vai "varrer" o array e buscar cada um dos seus objetos
 for (let i = 0; i < elementos.length; i++) {
-    //Pegando os itens do objeto dentro do array
+    
+    //Pegando o conteúdo dos itens "tag" e "texto" do objeto dentro do array por meio da "desestruturação"
     let {tag, texto} = elementos[i];
+    
     //Criando a variável "tagCriada" com o conteúdo do elemento "tag" de cada um dos indices
+    //EX: "<p></p>" | "<div></div>""
     let tagCriada = document.createElement(tag);
-    //Inserindo o texto dentro da "tag" recem capturada/criada
+
+    //Inserindo o conteúdo de "texto" dentro da "tag" recem capturada/criada
+    //EX: "<p> Frase 01 </p>"
     tagCriada.innerHTML = texto;
-    //Adicionando as linhas na constante "div" que foi criada apenas para "unificar" os textos do loop
+
+    //Adicionando as linhas na constante "div" que foi criada apenas para "unificar" os textos do loop.
+    //EX: "<p> Frase 01 </p><div> Frase 02 </div><section> Frase 03 </section><footer> Frase 04 </footer>"
     div.appendChild(tagCriada);
 }
 
-//Adicionando todas as "div" que foram "loopeadas" na constante "container" que fará a inserção no HTML
+
+//Utilizando o appendChild para inserir os valores da constante "div" na constante "container"
 container.appendChild(div)
