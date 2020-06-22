cc113db7fd2d24bd9cf63c7c863911d10701697f
Bruno Rodrigues <dominusbruno@gmail.com>
2020-06-22 09:39:11 -0300

    Exercício com NodeList/for (Browser)

--- 051 - Exercício com NodeList (Browser)/assets/js/main.js
+++ 051 - Exercício com NodeList (Browser)/assets/js/main.js
@@ -2,17 +2,18 @@
 
 //const paragrafo = document.querySelector('.paragrafo')
 
-//Capturamos todos os as tags <p></p> a tribuimos na constante "paragrafos"
+//Capturamos todos os as tags <p></p> e atribuimos na constante "paragrafos"
 const paragrafos = document.querySelectorAll('p');
 
 //Captura via DOM todo o corpo do HTML e atribui a constante "estiloBody"
 //const estiloBody = getComputedStyle(document.body);
 
-//Capturamos o "backgroundColor" 
+//Capturamos o "backgroundColor" e atribuimos a constante "backgroundColorBody"
 const backgroundColorBody = getComputedStyle(document.body).backgroundColor
 
 
-for (const p of paragrafos) {
-    p.style.backgroundColor = backgroundColorBody;
-    p.style.color = "#ffffff"
+//Varre os indices(i) de "paragrafos" alterando as cores de fundo e textode cada um deles
+for (const i of paragrafos) {
+    i.style.backgroundColor = backgroundColorBody;
+    i.style.color = "#ffffff"
 }
