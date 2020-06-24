c673a80bd2f19f1bd4ddcada174fa51eb184dc4f
Bruno Rodrigues <dominusbruno@gmail.com>
2020-06-24 13:23:19 -0300

    Tratando e lançando erros (try, catch, finally)

---
+++ 058 - Tratando e lançando erros (try, catch, finally)/index.js
@@ -0,0 +1,42 @@
+/*try {
+    console.log('Abri o arquivo')
+    console.log('Manipulei o arquivo')
+    errando // Erro qualquer
+    console.log('Fechei o arquivo') //Será "pulado"
+} catch (error) {
+    console.log('Tratando o erro')
+} finally {
+    console.log('FINALLY: Mesmo tendo código errado, Sempre sou executado')
+}
+*/
+
+
+
+
+//#################################################33333
+
+function retornaHora(data) {
+    if (data && !(data instanceof Date)){
+        throw new TypeError('Esperando instância de Date');
+    }
+
+    if (!data){
+        data = new Date();
+    }
+
+    return data.toLocaleTimeString('pt-BR',{
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    });
+}
+try {
+const data = new Date ('01-01-1970 12:58:12');
+const hora = retornaHora();
+console.log(hora);
+} catch (error) {
+    
+}finally{
+    console.log('Tenha um bom dia')
+}
