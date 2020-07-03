c151597b836de58cdcca3dcf7e08c4b06a354823
Bruno Rodrigues <dominusbruno@gmail.com>
2020-07-03 08:48:17 -0300

    Funções construtoras (constructor functions)

--- 071e072 - Funções construtoras (constructor functions)/assets/js/main.js
+++ 071e072 - Funções construtoras (constructor functions)/assets/js/main.js
@@ -1,29 +1,35 @@
-//Criando uma Factory Function (Função que retorna um OBJETO)
-function criaCalculadora(){
+const calculadora = new modeloCalculadora(); //Atribuindo a função a constante calculadora
+calculadora.inicia(); //Chamando o método "inicia()"
+
+function modeloCalculadora(){
     this.display = document.querySelector('.display');
 
     //Método inicia() para "startar" a calculadora
     this.inicia = () => {
-        this.clickBotoes()
+        this.capturaCliques()
         this.pressionaEnter()
         this.display.focus()
     };
 
-    //Método que captura o evento de pressionar no "ENTER" e executa o cálculo
-    this.pressionaEnter = () => {
-        this.display.addEventListener('keypress', e => {
-            if (e.keyCode === 13){
-                this.realizaConta()
-            }
-        })
-    };
+    //Método que vai capturar o evento/click dos botões executando rotinas ou outros
+    this.capturaCliques = () => {         
+        document.addEventListener('click', event => {
+            const el = event.target;
 
+            if (el.classList.contains('btn-num')) this.addNumDisplay(el)
+            if (el.classList.contains('btn-clear')) this.clear()
+            if (el.classList.contains('btn-del')) this.del()
+            if (el.classList.contains('btn-eq')) this.realizaConta()
+
+            this.display.focus()
+        });
+    };
+    
     //Método que executa o cálculo inserido no display
     this.realizaConta = () => {
-        let conta = this.display.value
+        let conta = eval(this.display.value)
 
         try { //Testa se a conta é válida (Tem números válidos)
-            conta = eval(conta)
             if(!conta){ //Se não existir números inseridos no display apresenta isso
                 alert('Conta inválida')
                 return
@@ -35,38 +41,22 @@ function criaCalculadora(){
         }
     };
 
+    //Método que envia/acrescenta os comandos ao display
+    this.addNumDisplay = el => this.display.value += el.innerText
 
-    //Método que vai capturar o evento/click dos botões
-    this.clickBotoes = () => {         
-        document.addEventListener('click', e => { //Captura o local do "click" no "document"
-            const el = e.target; //Salva o local do "click" na constante "el"
+    //Método do botão C, limpar o display.
+    this.clear = el => this.display.value = ''
 
-            if (el.classList.contains('btn-num')){//Verifica se o local do "click" contém a classe "btn-num"
-                this.btnParaDisplay(el.innerText)//Enviando o valor texto do elemento clickado para o método btnParaDisplay()
-            };
-            if (el.classList.contains('btn-clear')){//Verifica o click
-                this.display.value = '';//Atribui o valor do "display" a ''
-            };
-            if (el.classList.contains('btn-del')){
-                this.display.value = this.display.value.slice(0, -1); //Atribui o valor completo da string(0), e remove apenas o último número inserido(-1)
-            };
-            if (el.classList.contains('btn-eq')){
+    //Método que remove o último número/tecla digitado (backspace)
+    this.del = el => this.display.value = this.display.value.slice(0,-1)
+
+    //Método que captura o evento de pressionar no "ENTER" e executa o cálculo
+    this.pressionaEnter = () => {
+        this.display.addEventListener('keypress', e => {
+            if (e.keyCode === 13){
                 this.realizaConta()
-            };
-
-            this.display.focus()//Puxa o foco da calculadora para o "display"
-
-        });
+            }
+        })
     };
 
-    //Método que envia/acrescenta os comandos ao display
-    this.btnParaDisplay = (valor) => {
-        this.display.value += valor //Acrescenta o "valor" recebido na função ao atributo/variável display, que esse mostra na "tela" da calculadora.
-    }
 };
-
-
-//criaCalculadora().inicia()
-
-const calculadora = new criaCalculadora(); //Atribuindo a função a constante calculadora
-calculadora.inicia(); //Chamando o método "inicia()"
